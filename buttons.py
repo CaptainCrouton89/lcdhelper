40422ca16557a0fd7078a3ed820dc33174dc9789
CaptainCrouton89 <rhyneer.silas@gmail.com>
2021-12-03 22:02:43 -0900

    Added button script

--- buttons.py
+++ buttons.py
@@ -6,15 +6,25 @@ BUTTON_L_PIN = 11
 BUTTON_C_PIN = 13
 BUTTON_R_PIN = 15
 
-GPIO.setmode(GPIO.BOARD)             # choose BCM or BOARD  
-GPIO.setup(40, GPIO.OUT)           # set GPIO24 as an output   
-  
-try:  
-    while True:  
-        GPIO.output(40, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-        sleep(0.5)                 # wait half a second  
-        GPIO.output(40, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        sleep(0.5)                 # wait half a second  
-  
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-    GPIO.cleanup()                 # resets all GPIO ports used by this program
+def button_callback_L(channel):
+    print("L Button was pushed!")
+
+def button_callback_C(channel):
+    print("C Button was pushed!")
+
+def button_callback_R(channel):
+    print("R Button was pushed!")
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+GPIO.setup(BUTTON_L_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(BUTTON_C_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(BUTTON_R_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+GPIO.add_event_detect(BUTTON_L_PIN,GPIO.RISING,callback=button_callback_L) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(BUTTON_C_PIN,GPIO.RISING,callback=button_callback_C) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(BUTTON_R_PIN,GPIO.RISING,callback=button_callback_R) # Setup event on pin 10 rising edge
+
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
