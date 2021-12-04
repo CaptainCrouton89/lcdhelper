a8b7003739315bf21ea23341c0156681fc05a9b2
CaptainCrouton89 <rhyneer.silas@gmail.com>
2021-12-03 21:26:43 -0900

    Testing new script from different site

--- leds.py
+++ leds.py
@@ -1,27 +1,16 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 import random
 
-def setup_led_at_pin(pin):
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.setup(pin, GPIO.HIGH)
-    pwm = GPIO.PWM(pin, 2000)
-    pwm.start(0)
-    return pwm
-
-def setBrightness(pwm, val):  # 0 ~ 100 values since 0 ~ 100 only for duty cycle
-    pwm.ChangeDutyCycle(val)
-
-def close(all_pwm):
-    for pwm in all_pwm:
-        pwm.close()
-    GPIO.cleanup()
-
-def on(pwm):
-    GPIO.output(40, 1)
-
-GPIO.setmode(GPIO.BOARD)
-pwmR = setup_led_at_pin(40)
-on(pwmR)
-time.sleep(4)
-close(pwmR)
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
+GPIO.setup(40, GPIO.OUT)           # set GPIO24 as an output   
+  
+try:  
+    while True:  
+        GPIO.output(40, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        sleep(0.5)                 # wait half a second  
+        GPIO.output(40, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        sleep(0.5)                 # wait half a second  
+  
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup()                 # resets all GPIO ports used by this program  
